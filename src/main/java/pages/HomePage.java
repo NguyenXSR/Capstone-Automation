657088ac2e5e79bb80adc3a34a299af339606bf5
AnnTran <tranvinhthuyan9797@gmail.com>
2025-12-23 19:49:37 +0700

    OK

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,13 +1,18 @@
 package pages;
 
+//import pages.components.HomeMovieListing;
 import pages.components.HomeMovieSearch;
+import pages.components.MovieListingsSection;
 
 public class HomePage extends CommonPage {
 
-    private HomeMovieSearch movieSearch = new HomeMovieSearch();
+    private final HomeMovieSearch movieSearch = new HomeMovieSearch();
 
     public HomeMovieSearch getMovieSearch() {
         return movieSearch;
     }
+
+    private final MovieListingsSection movieListings = new MovieListingsSection();
+    public MovieListingsSection movieListings() { return movieListings; }
 
 }
--- src/test/java/testcases/homepage/HomeMovieSearchTest.java
+++ src/test/java/testcases/homepage/HomeMovieSearchTest.java
@@ -12,13 +12,12 @@ import reports.ExtentReportManager;
 
 @Listeners(TestListener.class)
 public class HomeMovieSearchTest extends BaseTest {
-    private HomePage homePage;
     private HomeMovieSearch movieSearch;
 
 
     @BeforeMethod
     public void setupPages() {
-        homePage = new HomePage();
+        HomePage homePage = new HomePage();
         movieSearch = homePage.getMovieSearch();
     }
 
@@ -33,36 +32,36 @@ public class HomeMovieSearchTest extends BaseTest {
         Assert.assertTrue(movieSearch.isShowtimeVisible(), "Showtime input is not visible");
     }
 
-    @Test
-    public void TC_Verify_movie_dropdown_lists_all_available_movies() {
-
-        ExtentReportManager.info("Check movie dropdown");
-        LOG.info("Check movie dropdown");
-        movieSearch.openMovieDropdown();
-        Assert.assertTrue(movieSearch.getMovieOptionTexts().size() > 0, "Movie dropdown has no items");
-    }
-
-    @Test
-    public void TC_Location_updates_dynamically_based_on_movie() {
-        ExtentReportManager.info("Check location update based on movie");
-        LOG.info("Check location update based on movie");
-
-        movieSearch.selectMovie("Man of Steel");
-        Assert.assertTrue(movieSearch.getLocationOptionTexts().size() > 0,
-                "Locations did not load after selecting movie");
-    }
-
-    @Test
-    public void TC_Showtime_updates_based_on_movie_and_location() {
-
-        ExtentReportManager.info("Check location update based on movie");
-        LOG.info("Check location update based on movie");
-
-        movieSearch.selectMovie("Man of Steel");
-        movieSearch.selectLocation("MegaGS - Cao Thắng");
-        Assert.assertTrue(movieSearch.getShowtimeOptionTexts().size() > 0,
-                "Showtimes did not load after selecting movie + location");
-    }
+//    @Test
+//    public void TC_Verify_movie_dropdown_lists_all_available_movies() {
+//
+//        ExtentReportManager.info("Check movie dropdown");
+//        LOG.info("Check movie dropdown");
+//        movieSearch.openMovieDropdown();
+//        Assert.assertTrue(movieSearch.getMovieOptionTexts().size() > 0, "Movie dropdown has no items");
+//    }
+//
+//    @Test
+//    public void TC_Location_updates_dynamically_based_on_movie() {
+//        ExtentReportManager.info("Check location update based on movie");
+//        LOG.info("Check location update based on movie");
+//
+//        movieSearch.selectMovie("Man of Steel");
+//        Assert.assertTrue(movieSearch.getLocationOptionTexts().size() > 0,
+//                "Locations did not load after selecting movie");
+//    }
+//
+//    @Test
+//    public void TC_Showtime_updates_based_on_movie_and_location() {
+//
+//        ExtentReportManager.info("Check location update based on movie");
+//        LOG.info("Check location update based on movie");
+//
+//        movieSearch.selectMovie("Man of Steel");
+//        movieSearch.selectLocation("MegaGS - Cao Thắng");
+//        Assert.assertTrue(movieSearch.getShowtimeOptionTexts().size() > 0,
+//                "Showtimes did not load after selecting movie + location");
+//    }
 
     @Test
     public void TC_Clicking_CTA_redirects_to_booking_flow() {
@@ -70,10 +69,20 @@ public class HomeMovieSearchTest extends BaseTest {
         ExtentReportManager.info("Check if booking flow success, will direct to purchase page");
         LOG.info("Check if booking flow success, will direct to purchase page");
 
+        ExtentReportManager.info("Step 1: select 'Phim' ");
+        LOG.info("Step 1: select movie");
         movieSearch.selectMovie("Man of Steel");
+
+        ExtentReportManager.info("Step 2: select 'Rạp'");
+        LOG.info("Step 2: select location");
         movieSearch.selectLocation("MegaGS - Cao Thắng");
+
+        ExtentReportManager.info("Step 3: select 'Giờ chiếu'");
+        LOG.info("SStep 3: select showtime");
         movieSearch.selectShowtime("03/10/2021 ~ 08:22");
 
+        ExtentReportManager.info("Step 4: click 'Mua vé' button");
+        LOG.info("SStep 3: select showtime");
         movieSearch.clickBuyTicketNow();
         Assert.assertTrue(driver.getCurrentUrl().contains("/purchase"), "Not redirected to ticket booking page");
 
